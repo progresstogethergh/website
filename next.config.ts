db956be1b609e2047f1edb7a14be1174548f45b0
Joshua Lees <82112905+jleesCY@users.noreply.github.com>
2026-08-26 00:32:48 -0400

    gh pages sample

--- next.config.ts
+++ next.config.ts
@@ -2,26 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["192.168.1.113"],
-  async headers() {
-    return [
-      {
-        source: '/(.*)',
-        headers: [
-          {
-            key: 'X-Content-Type-Options',
-            value: 'nosniff',
-          },
-          {
-            key: 'X-Frame-Options',
-            value: 'DENY',
-          },
-          {
-            key: 'Referrer-Policy',
-            value: 'strict-origin-when-cross-origin',
-          },
-        ],
-      },
-    ];
+  output: "export",
+  images: {
+    unoptimized: true, // Required for static export without a custom image loader
   },
 };
 
